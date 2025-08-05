12bf8ee19560c497773e7400b8fa05299d9368cd
Oskar  Golden <85715187+oskargolden@users.noreply.github.com>
2025-08-05 16:59:46 -0500

    game_object tests pass

--- tests/test_game_object.py
+++ tests/test_game_object.py
@@ -1,19 +1,22 @@
-import sys
-import os
-
-# Add the project root directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from game_logic.game_object import GameObject # ignore: type
+from game_logic.game_object import GameObject
 
 
 game_obj1 = GameObject('test_object', 'T', 'This is a test object')
 
 
 def test_game_object():
-    assert type(game_obj1.name) == str
-    assert type(game_obj1.symbol) == str
-    assert type(game_obj1.description) == str
-    
+    assert type(game_obj1.name) is str
+    assert type(game_obj1.symbol) is str
+    assert type(game_obj1.description) is str
+
+
 def test_game_object_repr():
-    assert repr(game_obj1) == "GameObject(name='test_object', symbol='T', description='This is a test object')"
+    """
+    Tests that the string representation of the GameObject is correct.
+    """
+    expected_repr = (
+        "GameObject(name='test_object', symbol='T', "
+        "description='This is a test object')"
+    )
+
+    assert repr(game_obj1) == expected_repr
